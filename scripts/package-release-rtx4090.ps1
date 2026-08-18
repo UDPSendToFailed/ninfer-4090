5301c093d061df7549bac57c930ea8d75d115171
UDPSendToFailed <udpsendtofailed@gmail.com>
2026-08-19 00:37:40 +0200

    chore(release): prepare v1.0.0 release

--- scripts/package-release-rtx4090.ps1
+++ scripts/package-release-rtx4090.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
-$ReleaseTag = 'v0.9.0'
+$ReleaseTag = 'v1.0.0'
 $RepoRoot = Split-Path -Parent $PSScriptRoot
 $BuildRoot = Join-Path $RepoRoot 'build-ninja'
 $DistRoot = Join-Path $RepoRoot 'dist'
